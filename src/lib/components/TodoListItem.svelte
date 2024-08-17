428df52a5b965c7e82ab73c36b7d9ef5eb7bb7c1
rio rifaldi <riorifaldi2004@gmail.com>
2024-08-17 14:58:19 +0700

    add todo update feature

--- src/lib/components/TodoListItem.svelte
+++ src/lib/components/TodoListItem.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import type { CustomeMouseEvent, TodosType } from '$lib/types';
-	import { IconTrash, IconEdit, IconCheck } from '@tabler/icons-svelte';
+	import type { CustomeKeyboardEvent, CustomeMouseEvent, TodosType } from '$lib/types';
+	import { IconTrash, IconEdit, IconCheck, IconDeviceFloppy } from '@tabler/icons-svelte';
+	import { onMount, tick } from 'svelte';
 	import { writable, type Writable } from 'svelte/store';
 
 	export let currentTodo: TodosType;
@@ -26,6 +27,36 @@
 		});
 	};
 
+	function handleEdit(e: CustomeKeyboardEvent) {
+		if (e.key !== 'Enter') return;
+		if (e.currentTarget.value === '') return;
+
+		store.update(($todos) => {
+			const currentIndex = $todos.findIndex((item) => item.id === currentTodo.id);
+			const { id, isChecked, todo } = currentTodo;
+
+			const newTodo = { id, isChecked, todo: e.currentTarget.value };
+			$todos.splice(currentIndex, 1, newTodo);
+			return $todos;
+		});
+		isEdit.set(false);
+	}
+
+	const handleUpdateWithButton = (e: CustomeMouseEvent) => {
+		let inputValue = input.value;
+		if (inputValue === '') return;
+
+		store.update(($todos) => {
+			const currentIndex = $todos.findIndex((item) => item.id === currentTodo.id);
+			const { id, isChecked, todo } = currentTodo;
+
+			const newTodo = { id, isChecked, todo: inputValue };
+			$todos.splice(currentIndex, 1, newTodo);
+			return $todos;
+		});
+		isEdit.set(false);
+	};
+
 	window.addEventListener('click', (e) => {
 		if (document.getElementById('todolist')?.contains(e.target as Node)) {
 			return;
@@ -33,6 +64,8 @@
 			isEdit.set(false);
 		}
 	});
+
+	let input: HTMLInputElement;
 </script>
 
 <div class="py-3 px-4 rounded-md w-full shadow flex justify-between">
@@ -41,36 +74,23 @@
 			type="checkbox"
 			name="check"
 			id="check"
-			class="size-5"
+			class="size-5 flex-shrink-0"
 			on:change={handleCheck}
 			checked={currentTodo.isChecked}
 		/>
-		{#if currentTodo.isChecked}
-			{#if $isEdit}
-				<input
-					type="text"
-					name="text update"
-					class="w-full focus:"
-					placeholder={`previous : ${currentTodo.todo}`}
-				/>
-			{:else}
-				<p class="line-through text-slate-400" on:dblclick|once={() => isEdit.set(!$isEdit)}>
-					{currentTodo.todo}
-				</p>
-			{/if}
-		{:else if $isEdit}
+
+		{#if $isEdit}
 			<input
 				type="text"
-				name="text update"
-				class="w-5/6 focus:outline-0 text-green-500"
-				placeholder={`previous : ${currentTodo.todo}`}
+				name="text-edit"
+				id="text-edit"
+				value={currentTodo.todo}
+				class="focus:outline-none focus:border-b-blue-700 border-b-blue-500 border-b-2 w-full"
+				bind:this={input}
+				on:keypress={handleEdit}
 			/>
 		{:else}
-			<p
-				on:dblclick|once={(e) => {
-					isEdit.set(!$isEdit);
-				}}
-			>
+			<p class={currentTodo.isChecked ? 'line-through text-slate-300' : ''}>
 				{currentTodo.todo}
 			</p>
 		{/if}
@@ -78,12 +98,27 @@
 
 	<div class="flex gap-5">
 		{#if $isEdit}
-			<button class="hover:bg-slate-200 rounded duration-200">
-				<IconCheck class="stroke-green-700" />
+			<button
+				class="hover:bg-slate-200 rounded duration-200 disabled:opacity-20 disabled:pointer-events-none"
+				on:click|stopPropagation={handleUpdateWithButton}
+			>
+				<IconDeviceFloppy class="hover:stroke-green-500" />
+			</button>
+		{:else}
+			<button
+				class="hover:bg-slate-200 rounded duration-200 disabled:opacity-20 disabled:pointer-events-none"
+				on:click|stopPropagation={async (e) => {
+					isEdit.set(!$isEdit);
+					await tick();
+					input.focus();
+				}}
+			>
+				<IconEdit class="stroke-blue-700" />
 			</button>
 		{/if}
+
 		<button on:click={handleDelete}>
-			<IconTrash class="stroke-red-700" />
+			<IconTrash class="stroke-red-500 duration-200 hover:stroke-red-700" />
 		</button>
 	</div>
 </div>
